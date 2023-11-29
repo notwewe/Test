97a33f7fe298cabde214d2c1a50444cc64f83d88
Francis Wedemeyer <francisdayagro24@gmail.com>
2023-11-29 20:22:05 +0800

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
+        System.out.print("Hello and welcome! (2)");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
